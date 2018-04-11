fe74b7496702334a70a6ff2a11c2c395d4778288
Steeve Vandecappelle <me@mizore.fr>
2018-04-11 15:45:46 +0200

    feat(lib_view) create lib view tree

--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
 
 import {AudioService} from './audio.service';
 import {Observable} from 'rxjs/Rx';
@@ -6,14 +7,14 @@ import {Observable} from 'rxjs/Rx';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
 
   public artists = [];
   public _page;
 
-  constructor(private _audioService: AudioService) { 
+  constructor(private _audioService: AudioService, private _sanitizer: DomSanitizer) { 
     this._page = 0;
   }
 
@@ -30,6 +31,10 @@ export class DashboardComponent implements OnInit {
     );
 
     this._page +=1;
+  }
+
+  getImage (src) {
+    return this._sanitizer.bypassSecurityTrustStyle(`linear-gradient(rgba(29, 29, 29, 0), rgba(16, 16, 23, 0.5)), url(${src})`);;
   }
 
 }
