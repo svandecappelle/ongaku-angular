9f7f501df7da811dc249c32c8a7969374a3f243d
Steeve Vandecappelle <me@mizore.fr>
2018-06-02 23:02:42 +0200

    fix(search) allow reset search

--- client/src/app/header/search-reducer.ts
+++ client/src/app/header/search-reducer.ts
@@ -6,11 +6,11 @@ import {
 
 export function searchReducer(state: String, action: SearchLibraryAction): String {
 
-  if (!state) {
+  if (state === undefined) {
     state = '';
   }
 
-  if (action && action.search) {
+  if (action && action.search !== undefined) {
     return action.search;
   } else {
     return state;
