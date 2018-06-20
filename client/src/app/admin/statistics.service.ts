c3d351a77b936fa4ae2bc2efce6474449a979e51
Steeve Vandecappelle <me@mizore.fr>
2018-06-20 06:28:28 +0200

    feat(amdin) graphs using chart.js

--- client/src/app/admin/statistics.service.ts
+++ client/src/app/admin/statistics.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class StatisticsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
+  getUsersAccess (): Observable<any> {
+    return this.http.get(`/api/admin/statistics/users/login`).map((data) => data)
+      .catch((error: any) => Observable.throw(error || 'Server error'));
+  }
 }
