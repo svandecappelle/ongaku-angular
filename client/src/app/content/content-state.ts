41e06c19308fcf3b4e776d6f5babadb4598d4b2a
Steeve Vandecappelle <me@mizore.fr>
2018-11-12 16:49:55 +0100

    Background view toggle with appstate

--- client/src/app/content/content-state.ts
+++ client/src/app/content/content-state.ts
@@ -8,10 +8,16 @@ export enum ToggleBackgroundType {
 export class ToggleBackgroundTypeAction implements Action {
     readonly type: ToggleBackgroundType;
 
-    constructor(type) {this.type = type;}
+    constructor(type) { this.type = type; }
 }
 
-export function backgroundUserReducer(useUserBackground: boolean, action: ToggleBackgroundTypeAction):boolean {
-    console.log(action);
-    return action.type === ToggleBackgroundType.USER_VIEWS;
+export function backgroundUserReducer(useUserBackground: boolean, action: ToggleBackgroundTypeAction): boolean {
+    switch (action.type) {
+        case ToggleBackgroundType.DYNAMIC:
+            return false;
+        case ToggleBackgroundType.USER_VIEWS:
+            return true;
+        default:
+            return useUserBackground;
+    }
 }
