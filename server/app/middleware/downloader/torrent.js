e0a1b68a074d10d9f8a9e0301d7a43be1bf1a94f
Steeve Vandecappelle <me@mizore.fr>
2019-11-07 15:09:00 +0100

    feat(torrent): add user possibility to dl

--- server/app/middleware/downloader/torrent.js
+++ server/app/middleware/downloader/torrent.js
@@ -22,31 +22,44 @@ class TorrentDownloader {
       persistent: true
     });
 
-    watcher
-      .on('add', (file) => {
-        if (path.extname(file) === ".torrent") {
-          console.log('File', file, 'has been added');
-          this.add(file);
-        }
-      });
+    watcher.on('add', (file) => {
+      if (path.extname(file) === ".torrent") {
+        this.add(file, false);
+      }
+    });
+
+    var watcherUsers = chokidar.watch(`${path.resolve(__dirname, "../../../../public/user")}`, {
+      ignored: /^\./,
+      persistent: true
+    });
+    watcherUsers.on('add', (file) => {
+      if (path.extname(file) === ".torrent") {
+        console.log('File', file, 'has been added');
+        this.add(file, true);
+      }
+    });
   }
 
-  add(file) {
-    var torrentContent = parseTorrent(fs.readFileSync(file));
+  add(file, userdir) {
+    const torrentContent = parseTorrent(fs.readFileSync(file));
+    const folderContainer = userdir ? path.dirname(file) : nconf.get('library')[0];
     this.client.add(torrentContent, (torrent) => {
       // Got torrent metadata!
-      console.log('Client is downloading:', torrent.infoHash)
-
       torrent.files.forEach((fileContent) => {
         let folder = fileContent.path.split(('/')).slice(0, -1).join("/");
         if (folder) {
           console.log("Creating dir: ", folder);
-          fs.mkdirSync(`${nconf.get('library')[0]}/${folder}`, { recursive: true });
+          fs.mkdirSync(`${folderContainer}/${folder}`, { recursive: true });
         }
-        var newPath = `${nconf.get('library')[0]}/${fileContent.path}`;
+        var newPath = `${folderContainer}/${fileContent.path}`;
         fs.writeFileSync(newPath, fileContent);
         // while still seeding need to make sure file.path points to the right place
       });
+
+      torrent.on('done', () => {
+        console.log('torrent download finished');
+        fs.unlinkSync(file);
+      })
     });
   }
 
